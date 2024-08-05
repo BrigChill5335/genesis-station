b2587f5720562cbae087114caeb33abab791a79e
Debiloid3000 <143325309+Debiloid3000@users.noreply.github.com>
2024-08-05 17:01:38 +0300

    Fix

--- Content.Shared/_Genesis/Psionics/Events.cs
+++ Content.Shared/_Genesis/Psionics/Events.cs
@@ -3,7 +3,7 @@ using Robust.Shared.Serialization;
 using Content.Shared.DoAfter;
 using Robust.Shared.GameStates;
 
-namespace Content.Shared._Genesis.Events;
+namespace Content.Shared._Genesis.Psionics.Events;
 
 [Serializable, NetSerializable]
 public sealed partial class PsionicRegenerationDoAfterEvent : DoAfterEvent
@@ -28,8 +28,32 @@ public sealed partial class GlimmerWispDrainDoAfterEvent : SimpleDoAfterEvent
 {
 }
 
+public sealed partial class PsionicInvisibilityPowerActionEvent : InstantActionEvent {}
+public sealed partial class PsionicInvisibilityPowerOffActionEvent : InstantActionEvent {}
 public sealed partial class HairballActionEvent : InstantActionEvent {}
 public sealed partial class EatMouseActionEvent : InstantActionEvent {}
+public sealed partial class MetapsionicPowerActionEvent : InstantActionEvent {}
+public sealed partial class TelegnosisPowerReturnActionEvent : InstantActionEvent {}
+public sealed partial class TelegnosisPowerActionEvent : InstantActionEvent {}
+public sealed partial class PsionicRegenerationPowerActionEvent : InstantActionEvent {}
+public sealed partial class NoosphericZapPowerActionEvent : EntityTargetActionEvent {}
+public sealed partial class DispelPowerActionEvent : EntityTargetActionEvent {}
+
+public sealed partial class DispelledEvent : HandledEntityEventArgs {}
+public sealed partial class MindSwapPowerActionEvent : EntityTargetActionEvent
+{
+
+}
+
+public sealed partial class MindSwapPowerReturnActionEvent : InstantActionEvent
+{
+
+}
+public sealed partial class PyrokinesisPowerActionEvent : EntityTargetActionEvent {}
+public sealed partial class PsychokinesisPowerActionEvent : WorldTargetActionEvent {}
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class MetapsionicVisibleComponent : Component
 {
 
 }
