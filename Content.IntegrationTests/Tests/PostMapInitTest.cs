a201a5d2711eb0c3794bf451247da493a0a56054
Debiloid3000 <143325309+Debiloid3000@users.noreply.github.com>
2024-08-19 14:36:42 +0300

    upd

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -49,6 +49,7 @@ namespace Content.IntegrationTests.Tests
         {
             // Genesis-Start
             "CentCommGenesis",
+            "GenesisCluster",
             // Genesis-End
             "Dev",
             "TestTeg",
