60da6ebd44b060f83c791b8d4c2d0c0b1950f2f6
Debiloid3000 <143325309+Debiloid3000@users.noreply.github.com>
2024-08-21 21:04:03 +0300

    Revert "yeah"
    This reverts commit 1ec7fa2dbcdf7e569aec1ffaa46192dc2de02789.

--- Content.Server/VoiceMask/VoiceMaskComponent.cs
+++ Content.Server/VoiceMask/VoiceMaskComponent.cs
@@ -14,12 +14,6 @@ public sealed partial class VoiceMaskComponent : Component
     [ViewVariables(VVAccess.ReadWrite)]
     public string VoiceName = "Unknown";
 
-    // Genesis-TTS-Start
-    [DataField]
-    [ViewVariables(VVAccess.ReadWrite)]
-    public string VoiceId = SharedHumanoidAppearanceSystem.DefaultVoice;
-    // Genesis-TTS-End
-
     /// <summary>
     /// If EnableSpeechVerbModification is true, overrides the speech verb used when this entity speaks.
     /// </summary>
